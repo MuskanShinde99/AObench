21ae0eaff6584a1da49a6a49204d30a640c229cd
MuskanShinde99 <muskan.shinde.99@gmail.com>
2025-07-14 14:58:41 +0200

    making a new file to create transformation matrices separately

---
+++ src/create_transformation_matrices.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 14 14:41:16 2025
+
+@author: ristretto-dao
+"""
+
+
+# Import Libraries
+from pathlib import Path
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+import sys
+
+
+# Configure root paths without changing the working directory
+OPT_LAB_ROOT = Path(os.environ.get("OPT_LAB_ROOT", "/home/ristretto-dao/optlab-master"))
+PROJECT_ROOT = Path(os.environ.get("PROJECT_ROOT", OPT_LAB_ROOT / "PROJECTS_3/RISTRETTO/Banc AO"))
+sys.path.append(str(OPT_LAB_ROOT))
+sys.path.append(str(PROJECT_ROOT))
+ROOT_DIR = PROJECT_ROOT
+
+# Import Specific Modules
+import dao
+from src.dao_setup import *  # Import all variables from setup
+from src.transformation_matrices_functions import * 
+from src.create_shared_memories import *
+
+Act2Phs, Phs2Act = compute_Act2Phs(nact, npix_small_pupil_grid, dm_modes_full, folder_transformation_matrices, verbose=True)
+
+# Create KL modes
+nmodes_kl = nact_valid
+Act2KL, KL2Act = compute_KL2Act(nact, npix_small_pupil_grid, nmodes_kl, dm_modes_full, small_pupil_mask, folder_transformation_matrices, verbose=True)
+KL2Phs, Phs2KL = compute_KL2Phs(nact, npix_small_pupil_grid, nmodes_kl, Act2Phs, Phs2Act, KL2Act, Act2KL, folder_transformation_matrices, verbose=True)
+
+# set shared memories
+KL2Act_shm.set_data(KL2Act)
+KL2Phs_shm.set_data(KL2Phs)
+
+# # Plot KL projected| on actuators
+# fig, axes = plt.subplots(2, 5, figsize=(15, 6))
+# # Flatten the axes array for easier indexing
+# axes_flat = axes.flatten()
+
+# for i, mode in enumerate(range(10)):
+#     im = axes_flat[i].imshow(KL2Act[mode].reshape(nact, nact), vmax=1, vmin=-1, cmap='viridis')
+#     axes_flat[i].set_title(f' KL2Act {mode}')
+#     axes_flat[i].axis('off')
+#     fig.colorbar(im, ax=axes_flat[i], fraction=0.03, pad=0.04)
+
+# plt.tight_layout()
+# plt.show()
--- src/dao_setup.py
+++ src/dao_setup.py
@@ -124,7 +124,7 @@ zernike_basis = np.asarray(zernike_basis)
 
 # [-0.0813878287964559, 0.09992195172893337]
 # Create a Tip-Tilt (TT) matrix with specified amplitudes as the diagonal elements
-tt_amplitudes = [-1.6740821497326528, 0.1306377981157021]  # Tip and Tilt amplitudes
+tt_amplitudes = [-1.6431675743243843, 0.15079571965398264]  # Tip and Tilt amplitudes
 tt_amplitude_matrix = np.diag(tt_amplitudes)
 tt_matrix = tt_amplitude_matrix @ zernike_basis[1:3, :]  # Select modes 1 (tip) and 2 (tilt)
 
