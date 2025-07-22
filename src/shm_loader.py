4c4ee996bb56d0bd67952ba15f86b27b7d7be621
MuskanShinde99 <muskan.shinde.99@gmail.com>
2025-07-22 13:51:11 +0200

    Load all shm paths from toml

--- src/shm_loader.py
+++ src/shm_loader.py
@@ -10,23 +10,12 @@ class ShmLoader:
         self.init_shm()
 
     def init_shm(self):
-        with open(self.toml_path, 'r') as f:
+        """Load all shared memory handles defined in the TOML file."""
+        with open(self.toml_path, "r") as f:
             shm_path = toml.load(f)
-        self.slopes_image_shm   = dao.shm(shm_path['slopes_image'])
-        self.phase_screen_shm   = dao.shm(shm_path['phase_screen'])
-        self.dm_phase_shm       = dao.shm(shm_path['dm_phase'])
-        self.phase_residuals_shm= dao.shm(shm_path['phase_residuals'])
-        self.normalized_psf_shm = dao.shm(shm_path['normalized_psf'])
-        self.commands_shm       = dao.shm(shm_path['commands'])
-        self.residual_modes_shm = dao.shm(shm_path['residual_modes'])
-        self.computed_modes_shm = dao.shm(shm_path['computed_modes'])
-        self.dm_kl_modes_shm    = dao.shm(shm_path['dm_kl_modes'])
-        self.delay_shm          = dao.shm(shm_path['delay'])
-        self.gain_shm           = dao.shm(shm_path['gain'])
-        self.leakage_shm        = dao.shm(shm_path['leakage'])
-        self.num_iterations_shm = dao.shm(shm_path['num_iterations'])
-        self.cam1_shm           = dao.shm(shm_path['cam1'])
-        self.cam2_shm           = dao.shm(shm_path['cam2'])
+
+        for key, path in shm_path.items():
+            setattr(self, f"{key}_shm", dao.shm(path))
 
 shm = ShmLoader()
 
