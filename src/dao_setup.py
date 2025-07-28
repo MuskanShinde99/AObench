03c212ff8396311e3af2510d2b180522370a8c9d
MuskanShinde99 <muskan.shinde.99@gmail.com>
2025-07-28 03:58:48 +0200

    dm flat

--- src/dao_setup.py
+++ src/dao_setup.py
@@ -169,7 +169,7 @@ data_pupil = data_pupil + data_focus
 
 # [-1.6510890005150187, 0.14406016044318903]
 # Create a Tip-Tilt (TT) matrix with specified amplitudes as the diagonal elements
-tt_amplitudes = [-0.6597515666225116, -0.024434728077832535] # Tip and Tilt amplitudes
+tt_amplitudes = [-1.6520855006768709, 0.08421487662444482] # Tip and Tilt amplitudes
 tt_amplitude_matrix = np.diag(tt_amplitudes)
 tt_matrix = tt_amplitude_matrix @ KL2Act[0:2, :]  # Select modes 1 (tip) and 2 (tilt)
 
