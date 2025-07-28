52fd26f6a465c3e8b9e0fc6c3d8320abb5999eea
MuskanShinde99 <muskan.shinde.99@gmail.com>
2025-07-28 16:10:19 +0200

    cleaning the dao setup file

--- scripts_with_dao/dao_calibrations_file.py
+++ scripts_with_dao/dao_calibrations_file.py
@@ -128,10 +128,10 @@ print(f'Number of valid pixels = {npix_valid}')
 set_data_dm(setup=setup)
 
 # Create shared memories that depends on number of valid pixels
-KL2PWFS_cube_shm = dao.shm('/tmp/KL2PWFS_cube.im.shm' , np.zeros((setup.nmodes_KL, setup.img_size_wfs_cam**2)), dtype=np.float64) 
-slopes_shm = dao.shm('/tmp/slopes.im.shm', np.zeros((npix_valid, 1)), dtype=np.uint32)
-KL2S_shm = dao.shm('/tmp/KL2S.im.shm' , np.zeros((setup.nmodes_KL, npix_valid)), dtype=np.float32)
-S2KL_shm = dao.shm('/tmp/S2KL.im.shm' , np.zeros((npix_valid, setup.nmodes_KL)), dtype=np.float32) 
+KL2PWFS_cube_shm = dao.shm('/tmp/KL2PWFS_cube.im.shm' , np.zeros((setup.nmodes_KL, setup.img_size_wfs_cam**2), dtype=np.float64))
+slopes_shm = dao.shm('/tmp/slopes.im.shm', np.zeros((npix_valid, 1), dtype=np.uint32))
+KL2S_shm = dao.shm('/tmp/KL2S.im.shm' , np.zeros((setup.nmodes_KL, npix_valid), dtype=np.float32))
+S2KL_shm = dao.shm('/tmp/S2KL.im.shm' , np.zeros((npix_valid, setup.nmodes_KL), dtype=np.float32))
 
 
 #%% Centering the PSF on the Pyramid Tip
--- src/dao_setup.py
+++ src/dao_setup.py
@@ -233,7 +233,7 @@ data_pupil = data_pupil + data_focus
 
 # [-1.6510890005150187, 0.14406016044318903]
 # Create a Tip-Tilt (TT) matrix with specified amplitudes as the diagonal elements
-tt_amplitudes = [-1.6687944845735057, 0.07128270224031574] # Tip and Tilt amplitudes
+tt_amplitudes = [-1.6561758672953513, 0.053487215450722214] # Tip and Tilt amplitudes
 tt_amplitude_matrix = np.diag(tt_amplitudes)
 tt_matrix = tt_amplitude_matrix @ KL2Act[0:2, :]  # Select modes 1 (tip) and 2 (tilt)
 
