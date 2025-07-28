308db49d4c71aa608b7f9cbcd8e49bea7bf19d2c
MuskanShinde99 <muskan.shinde.99@gmail.com>
2025-07-28 17:32:12 +0200

    preparing dao setup PAPYRUS

--- src/dao_setup.py
+++ src/dao_setup.py
@@ -6,7 +6,6 @@ PLACE_OF_TEST = os.environ.get("PLACE_OF_TEST", "Geneva").lower()
 if PLACE_OF_TEST == "Geneva":
     from src.dao_setup_Geneva import *  # noqa: F401,F403
     print('Were are in Geneva')
-    
 else:
     from src.dao_setup_PAPYRUS import *  # noqa: F401,F403
     las = None  # OHP setup has no laser control
