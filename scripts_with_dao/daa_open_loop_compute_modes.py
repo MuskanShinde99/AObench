7cb5e569c9c19cccfef40090925cdab51083a328
MuskanShinde99 <muskan.shinde.99@gmail.com>
2025-07-30 10:33:09 +0200

    adding script to compute open loop commands

--- scripts_with_dao/daa_open_loop_compute_modes.py
+++ scripts_with_dao/daa_open_loop_compute_modes.py
@@ -10,13 +10,29 @@ import time
 import os
 import matplotlib.pyplot as plt
 import tqdm
+from astropy.io import fits
 from PIL import Image
+import scipy
 from matplotlib.colors import LogNorm
 from src.utils import *
 from collections import deque
-from src.dao_setup import init_setup
-from src.utils import set_dm_actuators, set_data_dm
-from .shm_loader import shm
+
+
+# Import Specific Modules
+from src.config import config
+ROOT_DIR = config.root_dir
+import dao
+from src.utils import *
+from src.circular_pupil_functions import *
+from src.flux_filtering_mask_functions import *
+from src.tilt_functions import *
+from src.calibration_functions import *
+from src.kl_basis_eigenmodes_functions import computeEigenModes, computeEigenModes_notsquarepupil
+from src.transformation_matrices_functions import * 
+from src.psf_centring_algorithm_functions import *
+from src.shm_loader import shm
+from src.scan_modes_functions import scan_othermode_amplitudes
+from src.ao_loop_functions import *
 
 setup = init_setup()
 dm_phase_shm = shm.dm_phase_shm
@@ -29,6 +45,114 @@ computed_modes_shm = shm.computed_modes_shm
 commands_shm = shm.commands_shm
 dm_kl_modes_shm = shm.dm_kl_modes_shm
 dm_act_shm = shm.dm_act_shm
+
+num_iterations_shm = shm.num_iterations_shm
+gain_shm = shm.gain_shm
+leakage_shm = shm.leakage_shm
+delay_set_shm = shm.delay_set_shm
+
+#%% Creating and Displaying a Circular Pupil on the SLM
+
+# Display Pupil Data on SLM
+set_data_dm(setup=setup)
+
+#%% Load transformation matrices
+
+# From folder 
+KL2Act = fits.getdata(os.path.join(folder_transformation_matrices, f'KL2Act_nkl_{setup.nmodes_KL}_nact_{setup.nact}.fits'))
+KL2Phs = fits.getdata(os.path.join(folder_transformation_matrices, f'KL2Phs_nkl_{setup.nmodes_KL}_npupil_{setup.npix_small_pupil_grid}.fits'))
+
+# # From shared memories
+# KL2Act = KL2Act_shm.get_data()
+# KL2Phs = KL2Phs_shm.get_data()
+
+#%% Load Bias Image, Calibration Mask and Interaction Matrix
+
+# Load the bias image
+bias_filename = f'binned_bias_image.fits'
+bias_image = fits.getdata(os.path.join(folder_calib, bias_filename))
+print(f"Bias image shape: {bias_image.shape}")
+
+# Load the calibration mask for processing images.
+mask_filename = f'binned_mask_3s_pyr.fits'
+mask = fits.getdata(os.path.join(folder_calib, mask_filename))
+print(f"Mask dimensions: {mask.shape}")
+
+# Get valid pixel indices from the cropped mask
+valid_pixels_indices = np.where(mask > 0)
+
+# Load the response matrix 
+IM_filename = f'binned_response_matrix_KL2S_filtered_nact_{setup.nact}_amp_0.1_3s_pyr.fits'
+IM_KL2S = fits.getdata(os.path.join(folder_calib, IM_filename))  # /0.1
+
+RM_S2KL = np.linalg.pinv(IM_KL2S, rcond=0.10)
+print(f"Shape of the response matrix: {RM_S2KL.shape}")
+
+#%% Load Reference Image and PSF
+
+# Load reference image
+reference_image = fits.getdata(folder_calib / 'reference_image_raw.fits')
+normalized_reference_image = normalize_image(reference_image, mask, bias_image)
+pyr_img_shape = reference_image.shape
+print('Reference image shape:', pyr_img_shape)
+
+#Plot
+plt.figure()
+plt.imshow(reference_image)
+plt.colorbar()
+plt.title('Reference Image')
+plt.show()
+
+# Load Diffraction limited PSF
+diffraction_limited_psf = fits.getdata(folder_calib / 'reference_psf.fits')
+#diffraction_limited_psf /= diffraction_limited_psf.sum()
+fp_img_shape = diffraction_limited_psf.shape
+print('PSF shape:', fp_img_shape)
+
+#Radial Profile 
+plt.figure()
+plt.plot(diffraction_limited_psf[:,291:311:2])
+plt.plot(diffraction_limited_psf[286:316:2,:].T)
+plt.show()
+
+# Create the PSF mask 
+psf_mask, psf_center = create_psf_mask(diffraction_limited_psf, crop_size=100, radius=50)
+
+plt.figure()
+plt.imshow(psf_mask)
+plt.colorbar()
+plt.title('PSF Mask to compute strehl')
+plt.show()
+
+# Integrate the flux in that small region
+integrated_diff_psf = diffraction_limited_psf[psf_mask].sum()
+print('sum center PSF =', integrated_diff_psf)
+
+# Plot PSF with selected region overlayed
+plt.figure()
+plt.imshow(diffraction_limited_psf, norm=LogNorm(), cmap='viridis')
+plt.colorbar(label='Intensity')
+plt.title('PSF with Selected Region')
+circle = plt.Circle((psf_center[1], psf_center[0]), radius=50, color='red', fill=False, linewidth=2) # Overlay the integration region (circle)
+plt.gca().add_patch(circle)
+plt.show()
+
+# Compute the Strehl ratio
+#strehl_ratio = integrated_obs / integrated_diff
+
+#%% Defining the number of Kl modes used for Closed lop simulations
+
+plt.close('all')
+
+# Define KL modes to consider
+nmodes_kl = 175
+KL2Phs_new = KL2Phs[:nmodes_kl, :]
+Phs2KL_new = scipy.linalg.pinv(KL2Phs_new)
+KL2Act_new = KL2Act[:nmodes_kl, :]
+Act2KL_new = scipy.linalg.pinv(KL2Act_new)
+IM_KL2S_new = IM_KL2S[:nmodes_kl, :]
+RM_S2KL_new = np.linalg.pinv(IM_KL2S_new, rcond=0.10)
+
 
 
 # Load hardware and configuration parameters
@@ -68,8 +192,8 @@ valid_pixels_indices = np.where(mask > 0)
 
     
 # Initialize arrays to store Strehl ratio and total residual phase
-strehl_ratios = np.zeros(num_iterations)
-residual_phase_stds = np.zeros(num_iterations)
+# strehl_ratios = np.zeros(num_iterations)
+# residual_phase_stds = np.zeros(num_iterations)
 
 start_time = time.time()
 
@@ -106,7 +230,7 @@ while True:
     integrated_obs_psf = observed_psf[psf_mask].sum()
     strehl_ratio = integrated_obs_psf / integrated_diff_psf
     # strehl_ratio = np.max(observed_psf) / np.max(diffraction_limited_psf)
-    strehl_ratios[i] = strehl_ratio
+    # strehl_ratios[i] = strehl_ratio
 
     i += 1  # increment loop index
 
