fixing mode computation

scripts_with_dao/dao_AO_open_loop_with_kl_3s_pyr_papyrus.py
@@ -62,7 +62,7 @@ KL2Act_papy = KL2Act_papy_shm.get_data().T
 #%% Load Bias Image, Calibration Mask and Interaction Matrix
 
 # Load the bias image
-bias_filename = f'binned_bias_image.fits'
+bias_filename = f'bias_image.fits'
 bias_image = fits.getdata(os.path.join(folder_calib, bias_filename))
 print(f"Bias image shape: {bias_image.shape}")
 
@@ -70,7 +70,7 @@ print(f"Bias image shape: {bias_image.shape}")
 bias_image=np.zeros_like(bias_image)
 
 # Load the calibration mask for processing images.
-mask_filename = f'binned_mask_3s_pyr.fits'
+mask_filename = f'mask_3s_pyr.fits'
 mask = fits.getdata(os.path.join(folder_calib, mask_filename))
 print(f"Mask dimensions: {mask.shape}")
 
@@ -78,7 +78,7 @@ print(f"Mask dimensions: {mask.shape}")
 valid_pixels_indices = np.where(mask > 0)
 
 # Load the response matrix 
-IM_filename = f'binned_response_matrix_KL2S_filtered_nact_{setup.nact}_amp_0.1_3s_pyr.fits'
+IM_filename = f'response_matrix_KL2S_filtered_nact_{setup.nact}_amp_0.1_3s_pyr.fits'
 IM_KL2S = fits.getdata(os.path.join(folder_calib, IM_filename))  # /0.1
 
 RM_S2KL = np.linalg.pinv(IM_KL2S, rcond=0.10)
@@ -148,8 +148,6 @@ plt.close('all')
 # RM_S2KL_new = np.linalg.pinv(IM_KL2S_new, rcond=0.10)
 
 
-dm_papy_shm.set_data(KL2Act_papy[0])
-
  #%%   
 # Initialize arrays to store Strehl ratio and total residual phase
 # strehl_ratios = np.zeros(num_iterations)
@@ -164,18 +162,17 @@ while True:
         normalized_reference_image,
         setup=setup,
     )
-
     slopes = slopes_image[valid_pixels_indices].flatten()
     #fits.writeto(os.path.join(folder_gui, f'slopes_image.fits'), slopes_image, overwrite=True)
     
     # Compute KL modes present
     computed_modes = slopes @ RM_S2KL
     # multiply by two because this mode is computed for DM surface and we want DM phase
-    computed_modes_shm.set_data(computed_modes) # setting shared memory
+    computed_modes_shm.set_data(np.asanyarray(computed_modes).astype(np.float32)) # setting shared memory
     
     # Compute actuator commands
     act_pos = computed_modes @ KL2Act_papy
-    commands_shm.set_data(act_pos) # setting shared memory
+    commands_shm.set_data(np.asanyarray(act_pos).astype(np.float32)) # setting shared memory
 
     # Capture PSF
     fp_img = camera_fp.get_data()

scripts_with_dao/dao_calibrations_file.py
@@ -35,7 +35,6 @@ from src.ao_loop_functions import *
 
 #Loading setup
 setup = init_setup()
-setup = reload_setup()
 
 #Loading folder
 folder_calib = config.folder_calib
@@ -87,7 +86,7 @@ plt.colorbar()
 plt.show()
 
 # Save the Bias Image
-fits.writeto(os.path.join(folder_calib, f'binned_bias_image.fits'), np.asarray(bias_image), overwrite=True)
+fits.writeto(os.path.join(folder_calib, f'bias_image.fits'), np.asarray(bias_image), overwrite=True)
 
 #%% Turn on laser
 
@@ -107,6 +106,7 @@ else:
 # slm.set_data(data_slm)
 # time.sleep(wait_time)
 
+setup = reload_setup()
 # DM set to flat
 set_data_dm(setup=setup)
 dm_flat_papy_shm.set_data(setup.dm_flat.astype(np.float32))
@@ -131,14 +131,14 @@ KL2Act_papy = KL2Act_papy_shm.get_data().T
 #%% Creating a Flux Filtering Mask
 
 method='dm_random'
-flux_cutoff = 0.055 # 0.06 - papy dm random; 0.2 - geneva dm random
+flux_cutoff = 0.08 # 0.06 - papy dm random; 0.2 - geneva dm random
 modulation_angles = np.arange(0, 360, 1)  # angles of modulation
 modulation_amp = 15 # in lamda/D
 n_iter=800 # number of iternations for dm random commands
 
 mask = create_flux_filtering_mask(method, flux_cutoff, KL2Act_papy[0], KL2Act_papy[1],
                                modulation_angles, modulation_amp, n_iter,
-                               create_summed_image=False, verbose=False, verbose_plot=False)
+                               create_summed_image=False, verbose=False, verbose_plot=True)
 
 valid_pixels_mask_shm.set_data(mask)
 
@@ -178,7 +178,7 @@ scan_othermode_amplitudes_wfs_std(test_values, mode_index, mask,
 #%% Capture Reference Image
 
 #set bias image to zero for PAPY SIM tests
-bias_image=np.zeros_like(reference_image)
+bias_image=np.zeros_like(bias_image)
 
 #Timestamp
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -212,6 +212,8 @@ n_frames=20
 fp_image = (np.mean([camera_fp.get_data().astype(np.float32) for i in range(n_frames)], axis=0)).astype(camera_fp.get_data().dtype)
 reference_psf_shm.set_data(fp_image)
 fits.writeto(folder_calib / 'reference_psf.fits', fp_image, overwrite=True)
+fits.writeto(folder_calib / f'reference_psf_{timestamp}.fits', reference_image, overwrite=True)
+
 
 #Display the PSF
 plt.figure()
@@ -236,7 +238,7 @@ calibration_repetitions = 2
 #mode_repetitions = {0: 10, 3: 10} # Repeat the 0th mode ten times, the 3rd mode ten times, rest default to 1
 #mode_repetitions = [2, 3]  # Repeat the 0th mode twice, the 1st mode three times, beyond the 1st default to 1
 
-mode_repetitions = {0: 30, 1: 30, 10: 5}
+mode_repetitions = {0: 30, 1: 30}
 
 # Run calibration and compute matrices
 # use the ref img, mask directly from shared memories 

src/calibration_functions.py
@@ -135,7 +135,6 @@ def perform_push_pull_calibration_with_phase_basis(
                         normalized_reference_image,
                         setup=setup,
                     )
-                    slopes_image_shm.set_data(slopes_image) # setting the shared memory separately because setting inside the function is not working
 
                     t9 = time.time()
 
@@ -225,6 +224,7 @@ def create_response_matrix(
     phase_amp,
     reference_image,
     mask,
+    bias_image,
     *,
     verbose=True,
     verbose_plot=False,
@@ -291,6 +291,7 @@ def create_response_matrix(
             phase_amp,
             reference_image,
             mask,
+            bias_image,
             verbose=verbose,
             verbose_plot=verbose_plot,
             mode_repetitions=mode_repetitions,
@@ -319,9 +320,9 @@ def create_response_matrix(
     response_matrix_filtered = compute_response_matrix(push_pull_images, mask).astype(np.float32)
 
     # Define output filenames
-    pull_filename     = f'binned_processed_response_cube_KL2PWFS_only_pull_nact_{nact}_amp_{phase_amp}_3s_pyr.fits'
-    push_filename     = f'binned_processed_response_cube_KL2PWFS_only_push_nact_{nact}_amp_{phase_amp}_3s_pyr.fits'
-    pushpull_filename = f'binned_processed_response_cube_KL2PWFS_push-pull_nact_{nact}_amp_{phase_amp}_3s_pyr.fits'
+    pull_filename     = f'processed_response_cube_KL2PWFS_only_pull_nact_{nact}_amp_{phase_amp}_3s_pyr.fits'
+    push_filename     = f'processed_response_cube_KL2PWFS_only_push_nact_{nact}_amp_{phase_amp}_3s_pyr.fits'
+    pushpull_filename = f'processed_response_cube_KL2PWFS_push-pull_nact_{nact}_amp_{phase_amp}_3s_pyr.fits'
 
     # Save FITS files
     if verbose: print('Pull images saved')
@@ -337,15 +338,15 @@ def create_response_matrix(
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     
     # Filterd
-    filtered = f'binned_response_matrix_KL2S_filtered_nact_{nact}_amp_{phase_amp}_3s_pyr.fits'
-    filtered_timestamped = f'binned_response_matrix_KL2S_filtered_nact_{nact}_amp_{phase_amp}_3s_pyr_{timestamp}.fits'
+    filtered = f'response_matrix_KL2S_filtered_nact_{nact}_amp_{phase_amp}_3s_pyr.fits'
+    filtered_timestamped = f'response_matrix_KL2S_filtered_nact_{nact}_amp_{phase_amp}_3s_pyr_{timestamp}.fits'
     fits.writeto(os.path.join(folder_calib, filtered), response_matrix_filtered, overwrite=True)
     fits.writeto(os.path.join(folder_calib, filtered_timestamped), response_matrix_filtered, overwrite=True)
     if verbose: print(f"Filtered matrix saved to:\n  {filtered}\n  {filtered_timestamped}")
     
     # Full
-    full = f'binned_response_matrix_KL2S_full_nact_{nact}_amp_{phase_amp}_3s_pyr.fits'
-    full_timestamped = f'binned_response_matrix_KL2S_full_nact_{nact}_amp_{phase_amp}_3s_pyr_{timestamp}.fits'
+    full = f'response_matrix_KL2S_full_nact_{nact}_amp_{phase_amp}_3s_pyr.fits'
+    full_timestamped = f'response_matrix_KL2S_full_nact_{nact}_amp_{phase_amp}_3s_pyr_{timestamp}.fits'
     fits.writeto(os.path.join(folder_calib, full), response_matrix_full, overwrite=True)
     fits.writeto(os.path.join(folder_calib, full_timestamped), response_matrix_full, overwrite=True)
     if verbose: print(f"Full matrix saved to:\n  {full}\n  {full_timestamped}")

src/utils.py
@@ -414,7 +414,7 @@ def get_slopes_image(mask, bias_image, normalized_reference_image, pyr_img=None,
     slopes_image = compute_pyr_slopes(normalized_pyr_img, normalized_reference_image)
     # print('slopes_image data type', slopes_image.dtype)
     # print('slopes_image shape', slopes_image.shape)
-    slopes_image_shm.set_data(slopes_image)
+    slopes_image_shm.set_data(slopes_image.astype(np.float64))
     return slopes_image
 
 