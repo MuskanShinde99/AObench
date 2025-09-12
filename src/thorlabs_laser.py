05f64cc8699de64192b37c8886295999fe99b94b
MuskanShinde99 <muskan.shinde.99@gmail.com>
2025-09-12 17:20:09 +0200

    make remote and local branch same

--- src/thorlabs_laser.py
+++ src/thorlabs_laser.py
@@ -8,7 +8,7 @@ Created on Thu Oct 17 15:56:47 2024
 from DEVICES_3.Thorlabs.MCLS1 import mcls1
 
 channel = 1
-state = 0 # 0 to turn OFF, 1 to turn ON
+state = 1 # 0 to turn OFF, 1 to turn ON
 las = mcls1("/dev/ttyUSB0")
 las.set_channel(channel)
 las.enable(state) 
