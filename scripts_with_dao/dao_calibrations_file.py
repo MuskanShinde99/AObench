66b8c463d4a7ccfd269f3953a70445baeb77555e
MuskanShinde99 <muskan.shinde.99@gmail.com>
2025-09-12 15:09:08 +0200

    Add files via upload

---
+++ scripts_with_dao/dao_calibrations_file.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 14:49:20 2025
+
+@author: laboptic
+"""
+
+# Standard library
+import os
+import time
+from datetime import datetime
+import numpy as np
+from astropy.io import fits
+from matplotlib import pyplot as plt
+import importlib
+
+# Import Specific Modules
+import dao
+from src.dao_setup import init_setup, las
+from src.utils import set_data_dm, reload_setup
+
+#Loading setup
+setup = init_setup()
+setup = reload_setup()
+
+from src.config import config
+from src.utils import *
+from src.circular_pupil_functions import *
+from src.flux_filtering_mask_functions import *
+from src.tilt_functions import *
+from src.calibration_functions import *
+from src.kl_basis_eigenmodes_functions import computeEigenModes, computeEigenModes_notsquarepupil
+from src.transformation_matrices_functions import * 
+from src.psf_centring_algorithm_functions import *
+from src.shm_loader import shm
+from src.scan_modes_functions import *
+from src.ao_loop_functions import *
+
+#Loading folder
+folder_calib = config.folder_calib
+
+#Loading shared memories
+bias_image_shm = shm.bias_image_shm
+valid_pixels_mask_shm = shm.valid_pixels_mask_shm
+npix_valid_shm = shm.npix_valid_shm
+reference_image_shm = shm.reference_image_shm
+normalized_ref_image_shm = shm.normalized_ref_image_shm
+reference_psf_shm = shm.reference_psf_shm
+KL2Act_papy_shm = shm.KL2Act_papy_shm
+dm_flat_papy_shm = shm.dm_flat_papy_shm
+dm_papy_shm = shm.dm_papy_shm
+
+
+# #%% Accessing Devices
+
+# # Initialize Cameras
+# camera_wfs = setup.camera_wfs
+# camera_fp = setup.camera_fp
+
+# # Intialize DM
+# deformable_mirror = setup.defomable_mirror
+    
+#%%
+# Load the bias image
+bias_filename = f'bias_image.fits'
+bias_image = fits.getdata(os.path.join(folder_calib, bias_filename))
+print(f"Bias image shape: {bias_image.shape}")
+
+#%% Setting DM to flat
+
+# Compute and display Pupil Data on SLM
+# data_slm = compute_data_slm()
+# slm.set_data(data_slm)
+# time.sleep(wait_time)
+
+# DM set to flat
+set_data_dm(setup=setup)
+#dm_flat_papy_shm.set_data(setup.dm_flat.astype(np.float32))
+#fits.writeto(folder_calib / 'dm_flat_papy.fits', setup.dm_flat.astype(np.float32), overwrite=True)
+
+#%% Load transformation matrices
+
+# # Load transformation matrices from shared memories
+# KL2Act = KL2Act_shm.get_data()
+# KL2Phs = KL2Phs_shm.get_data()
+
+# From folder 
+# KL2Act = fits.getdata(os.path.join(folder_transformation_matrices, f'KL2Act_nkl_{setup.nmodes_KL}_nact_{setup.nact}.fits'))
+# KL2Phs = fits.getdata(os.path.join(folder_transformation_matrices, f'KL2Phs_nkl_{setup.nmodes_KL}_npupil_{setup.npix_small_pupil_grid}.fits'))
+
+KL2Act_papy = KL2Act_papy_shm.get_data().T
+
+# plt.figure()
+# plt.plot(KL2Act_papy[1,:])
+# plt.show()
+
+#%% Creating a Flux Filtering Mask
+
+# Load the calibration mask for processing images.
+mask_filename = f'mask_3s_pyr.fits'
+mask = fits.getdata(os.path.join(folder_calib, mask_filename))
+print(f"Mask dimensions: {mask.shape}")
+
+valid_pixels_mask_shm.set_data(mask)
+
+# Get valid pixels from the mask
+#put with in the function to create mask
+valid_pixels_indices = np.where(mask > 0)
+npix_valid = valid_pixels_indices[0].shape[0]
+npix_valid_shm.set_data(np.array([[npix_valid]]))
+print(f'Number of valid pixels = {npix_valid}')
+
+#Reset the DM to flat
+set_data_dm(setup=setup)
+
+# Create shared memories that depends on number of valid pixels
+KL2PWFS_cube_shm = dao.shm('/tmp/KL2PWFS_cube.im.shm' , np.zeros((setup.nmodes_KL, setup.img_size_wfs_cam_x*setup.img_size_wfs_cam_y), dtype=np.float64))
+slopes_shm = dao.shm('/tmp/slopes.im.shm', np.zeros((npix_valid, 1), dtype=np.uint64))
+KL2S_shm = dao.shm('/tmp/KL2S.im.shm' , np.zeros((setup.nmodes_KL, npix_valid), dtype=np.float64))
+S2KL_shm = dao.shm('/tmp/S2KL.im.shm' , np.zeros((npix_valid, setup.nmodes_KL), dtype=np.float64))
+
+
+#%% Centering the PSF on the Pyramid Tip
+
+# print('Start centering algorithm')
+# center_psf_on_pyramid_tip(mask=mask, 
+#                           bounds = [(-2.0, 2.0), (-2.0, 2.0)], variance_threshold=0.1, 
+#                           update_setup_file=True, verbose=True, verbose_plot=True)
+
+ #%% Scanning modes to find zero of the pyramid
+
+# test_values = np.arange(-0.5, 0.5, 0.05)
+# mode_index = 3 # 0 - focus, 1 - astimgatism, 2 -astigmatism 
+# #scan_othermode_amplitudes(test_values, mode_index, update_setup_file=True)
+# scan_othermode_amplitudes_wfs_std(test_values, mode_index, mask, 
+#                                   update_setup_file=False)
+  
+# #revise the crieteria to standard deviation of intensities within the valid pixels
+
+#%% Capture Reference Image
+
+#Timestamp
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# Compute and display Pupil Data on SLM
+set_data_dm(setup=setup)
+
+# Capure the Reference Image
+n_frames=1000
+reference_image = (np.mean([camera_wfs.get_data().astype(np.float32) for i in range(n_frames)], axis=0)).astype(camera_wfs.get_data().dtype)
+#reference_image = fits.getdata(os.path.join(folder_calib, f'mask_3s_pyr.fits'))
+# average over several frames
+reference_image_shm.set_data(reference_image)
+fits.writeto(folder_calib / 'reference_image_raw.fits', reference_image, overwrite=True)
+fits.writeto(folder_calib / f'reference_image_raw_{timestamp}.fits', reference_image, overwrite=True)
+
+#Plot
+plt.figure()
+plt.imshow(reference_image)
+plt.colorbar()
+plt.title('Reference Image')
+plt.show()
+
+# Display the Focal plane image
+n_frames=20
+fp_image = (np.mean([camera_fp.get_data().astype(np.float32) for i in range(n_frames)], axis=0)).astype(camera_fp.get_data().dtype)
+reference_psf_shm.set_data(fp_image)
+fits.writeto(folder_calib / 'reference_psf.fits', fp_image, overwrite=True)
+fits.writeto(folder_calib / f'reference_psf_{timestamp}.fits', reference_image, overwrite=True)
+
+
+#Display the PSF
+plt.figure()
+plt.imshow(fp_image) 
+plt.colorbar()
+plt.title('PSF')
+plt.show()
+
+# Display the radial profile of the PSF
+plt.figure()
+plt.plot(fp_image[:, 253:273])
+plt.title('PSF radial profile')
+plt.show()
+
+#%%
+phase_amp = 0.05
+
+# Number of times to repeat the whole calibration
+calibration_repetitions = 1
+
+# Modes repition dictionary
+#mode_repetitions = {0: 10, 3: 10} # Repeat the 0th mode ten times, the 3rd mode ten times, rest default to 1
+#mode_repetitions = [2, 3]  # Repeat the 0th mode twice, the 1st mode three times, beyond the 1st default to 1
+
+mode_repetitions = [50]*150
+#mode_repetitions = [200] * setup.nmodes_KL
+
+# Run calibration and compute matrices
+# use the ref img, mask directly from shared memories 
+response_matrix_full, response_matrix_filtered = create_response_matrix(
+    KL2Act_papy,
+    phase_amp,
+    reference_image,
+    mask,
+    bias_image,
+    verbose=True,
+    verbose_plot=False,
+    calibration_repetitions=calibration_repetitions,
+    mode_repetitions=mode_repetitions,
+    push_pull=False,
+    pull_push=True,
+    n_frames=100,
+)
+
+#Reset the DM to flat
+set_data_dm(setup=setup)
+
+#response_matrix_filtered = response_matrix_full[:, mask.ravel() > 0]
+
+# Print shapes ---
+print("Full response matrix shape:    ", response_matrix_full.shape)
+print("Filtered response matrix shape:", response_matrix_filtered.shape)
+
+# Plot filtered matrix 
+plt.figure()
+plt.imshow(response_matrix_filtered, cmap='gray', aspect='auto')
+plt.title('Filtered Push-Pull Response Matrix')
+plt.xlabel('Slopes')
+plt.ylabel('Modes')
+plt.colorbar()
+plt.show()
+
+#saving the flattened push-pull images in shared memory
+KL2PWFS_cube_shm.set_data(np.asanyarray(response_matrix_full).astype(np.float64))
+KL2S_shm.set_data(np.asanyarray(response_matrix_filtered).astype(np.float64))
+
---
+++ scripts_with_dao/dao_linearity_curve_3s_pyr_papyrus.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 14 13:33:29 2025
+
+@author: laboptic
+"""
+# Standard library
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import scipy
+from matplotlib.colors import LogNorm
+
+# Import Specific Modules
+from src.config import config
+from src.dao_setup import init_setup
+from src.utils import *
+from src.shm_loader import shm
+
+# Flag to identify on-sky data
+OnSky = False
+suffix = "_OnSky" if OnSky else ""
+
+#Loading setup
+setup = init_setup()
+setup = reload_setup()
+
+#Loading shared memories
+dm_phase_shm = shm.dm_phase_shm
+phase_screen_shm = shm.phase_screen_shm
+phase_residuals_shm = shm.phase_residuals_shm
+residual_modes_shm = shm.residual_modes_shm
+slopes_image_shm = shm.slopes_image_shm
+normalized_psf_shm = shm.normalized_psf_shm
+computed_modes_shm = shm.computed_modes_shm
+commands_shm = shm.commands_shm
+strehl_ratio_shm = shm.strehl_ratio_shm
+norm_flux_pyr_img_shm = shm.norm_flux_pyr_img_shm
+
+dm_kl_modes_shm = shm.dm_kl_modes_shm
+dm_act_shm = shm.dm_act_shm
+dm_flat_papy_shm = shm.dm_flat_papy_shm
+KL2Act_papy_shm = shm.KL2Act_papy_shm
+dm_papy_shm = shm.dm_papy_shm
+
+#Loading folder
+folder_calib = config.folder_calib
+folder_gui = setup.folder_gui
+
+# Load hardware and configuration parameters
+camera_wfs = setup.camera_wfs
+camera_fp = setup.camera_fp
+npix_small_pupil_grid = setup.npix_small_pupil_grid
+
+
+#%% Creating and Displaying a Circular Pupil on the SLM
+
+#DM set to flat
+set_data_dm(setup=setup)
+#dm_flat_papy_shm.set_data(setup.dm_flat.astype(np.float32))
+#fits.writeto(folder_calib / 'dm_flat_papy.fits', setup.dm_flat.astype(np.float32), overwrite=True)
+
+
+#%% Load transformation matrices
+
+KL2Act_papy = KL2Act_papy_shm.get_data().T
+
+#%% Load Bias Image, Calibration Mask and Interaction Matrix
+
+# Load the bias image
+bias_filename = f'bias_image.fits'
+bias_image = fits.getdata(os.path.join(folder_calib, bias_filename))
+print(f"Bias image shape: {bias_image.shape}")
+
+# Set bias image to zero for PAPY SIM tests
+# bias_image=np.zeros_like(bias_image) #TODO: Remove it
+
+# Load the calibration mask for processing images.
+mask_filename = f'mask_3s_pyr{suffix}.fits'
+mask = fits.getdata(os.path.join(folder_calib, mask_filename))
+print(f"Mask dimensions: {mask.shape}")
+
+# Get valid pixel indices from the cropped mask
+valid_pixels_indices = np.where(mask > 0)
+
+# Load the response matrix 
+IM_filename = f'processed_response_cube_KL2PWFS_push-pull_nact_17_amp_0.05_3s_pyr.fits'
+IM_KL2S_full = fits.getdata(os.path.join(folder_calib, IM_filename))  # /0.1
+IM_KL2S = compute_response_matrix(IM_KL2S_full, mask).astype(np.float32)
+
+RM_S2KL = np.linalg.pinv(IM_KL2S, rcond=0.10)
+print(f"Shape of the reconstruction matrix: {RM_S2KL.shape}")
+
+
+#%% Load Reference Image and PSF
+
+# Load reference image
+reference_image = fits.getdata(folder_calib / 'reference_image_raw.fits')
+#reference_image = fits.getdata(os.path.join(folder_calib, f'mask_3s_pyr.fits'))
+#bias_image_old = fits.getdata(folder_calib / 'bias_image_medium_gain_500Hz.fits')
+normalized_reference_image = normalize_image(reference_image, mask, bias_image)
+pyr_img_shape = reference_image.shape
+print('Reference image shape:', pyr_img_shape)
+
+#Plot
+plt.figure()
+plt.imshow(normalized_reference_image)
+plt.colorbar()
+plt.title('Normalized Reference Image')
+plt.show()
+
+#%% Select number of KL modes to consider
+
+# nmodes_KL = 195
+# IM_KL2S = IM_KL2S[:nmodes_KL,:]
+# RM_S2KL = np.linalg.pinv(IM_KL2S, rcond=0.10)
+# print(f"Shape of the response matrix: {RM_S2KL.shape}")
+
+#%%  Compute linearity curves
+
+plt.close('all')
+
+# Number of KL modes to plot
+num_modes = 10
+applied_phase_amp = np.arange(-1.0, 1.1, 0.1) # 
+computed_phase_amp = np.zeros((num_modes, len(applied_phase_amp)))
+
+# Loop through each Zernike mode
+for j, mode in enumerate(np.arange(0, 10, 1)):
+    
+    mode = mode
+    print(f"Applying phase for mode: {mode}")
+    
+    for i, amp in enumerate(applied_phase_amp):
+
+        # Compute the KL mode actuator positions
+        kl_mode = amp * KL2Act_papy[mode]
+
+        # Put KL mode on the DM
+        set_data_dm(kl_mode, setup=setup,)
+
+        # Capture image and compute slopes
+        slopes_image = get_slopes_image(
+            mask,
+            bias_image,
+            normalized_reference_image,
+            setup=setup,
+        )
+
+        slopes = slopes_image[valid_pixels_indices].flatten()
+
+        # Compute modes using the response matrix
+        computed_modes = slopes @ RM_S2KL 
+        computed_modes_shm.set_data(np.asanyarray(computed_modes).astype(np.float32)) # setting shared memory
+
+        # Store computed modes for this mode
+        computed_phase_amp[j, i] = computed_modes[mode]
+        
+
+# Plot the results in subplots
+fig, axes = plt.subplots(nrows=2, ncols=5, figsize=(20, 8))
+axes = axes.flatten()
+
+for mode in range(num_modes):
+    axes[mode].plot(applied_phase_amp, computed_phase_amp[mode], label='Computed Phase Amplitude')  # Adjust as needed
+    axes[mode].plot(applied_phase_amp, applied_phase_amp, label='y = x', linestyle='--')
+    axes[mode].set_xlabel('Applied Phase Amplitude [λ]')
+    axes[mode].set_ylabel('Reconstructed Phase Amplitude [λ]')
+    axes[mode].set_title(f'KL mode {mode}')
+    axes[mode].legend()
+    #axes[mode].set_ylim(-0.35, 0.35)
+    axes[mode].grid(True)
+
+plt.tight_layout()
+
+
+# Save to file
+save_path = folder_calib / "linearity_curves.png"
+plt.savefig(save_path, dpi=300)
+plt.show()
+
+print(f"Linearity curves saved to: {save_path}")
+
+#DM set to flat
+set_data_dm(setup=setup)
+
+#%% Compute cross correlation matrix
+
+# Parameters
+num_modes = 195
+amp = 0.1 # Use only a single amplitude
+crosscorrelation_matrix = np.zeros((num_modes, num_modes))
+
+# For storing full reconstructed vectors if needed
+reconstructed_all = np.zeros((num_modes, num_modes))
+
+# Loop over each mode
+for mode in range(num_modes):
+    print(f"Applying KL mode {mode}")
+    
+    # Generate DM command for this KL mode
+    kl_mode = amp * KL2Act_papy[mode]
+
+    # Apply DM command
+    set_data_dm(kl_mode, setup=setup)
+
+    # Capture image and compute slopes
+    slopes_image = get_slopes_image(
+        mask,
+        bias_image,
+        normalized_reference_image,
+        setup=setup,
+    )
+
+    slopes = slopes_image[valid_pixels_indices].flatten()
+
+    # Reconstruct the phase vector in KL basis
+    computed_modes = slopes @ RM_S2KL
+    computed_modes_shm.set_data(np.asanyarray(computed_modes).astype(np.float32))
+
+    # Save reconstructed KL amplitudes
+    reconstructed_all[mode, :] = computed_modes
+
+# # Compute cross-correlation matrix between input and output modes
+# for i in range(num_modes):
+#     for j in range(num_modes):
+#         num = np.dot(reconstructed_all[i], reconstructed_all[j])
+#         den = np.linalg.norm(reconstructed_all[i]) * np.linalg.norm(reconstructed_all[j])
+#         crosscorrelation_matrix[i, j] = num / den if den != 0 else 0
+
+crosscorrelation_matrix = reconstructed_all/amp
+
+# Optionally plot the cross-correlation matrix
+plt.figure(figsize=(8, 6))
+plt.imshow(crosscorrelation_matrix, cmap='viridis', interpolation='nearest')
+plt.colorbar(label='Cross-correlation')
+plt.title(f'Cross-correlation matrix (Amp = {amp})')
+plt.xlabel('Reconstructed KL mode index')
+plt.ylabel('Applied KL mode index')
+plt.grid(False)
+plt.tight_layout()
+plt.show()
+
+# Save cross-correlation matrix as FITS
+fits_path = folder_calib / f"crosscorrelation_matrix.fits"
+fits.writeto(fits_path, crosscorrelation_matrix, overwrite=True)
+
+print(f"Cross-correlation matrix saved to: {fits_path}")
+
+#DM set to flat
+set_data_dm(setup=setup)
