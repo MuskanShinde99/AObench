a24211c3bcb42f27e93754d64fab11a0852d85f2
MuskanShinde99 <muskan.shinde.99@gmail.com>
2026-01-09 11:47:59 +0100

    error file

---
+++ src/untitled1.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  9 11:43:41 2026
+
+@author: ristretto-dao
+"""
+
+Python 3.12.9 | packaged by Anaconda, Inc. | (main, Feb  6 2025, 18:56:27) [GCC 11.2.0]
+Type "copyright", "credits" or "license" for more information.
+
+IPython 8.36.0 -- An enhanced Interactive Python. Type '?' for help.
+
+In [1]: %runcell -i 0 '/home/ristretto-dao/optlab-master/PROJECTS_3/RISTRETTO/Banc AO/scripts_with_dao/dao_calibrations_file.py'
+Were are in Geneva
+2026-01-09_11:08:55:861638 [info] dao.c:daoShmShm2Img:707: shm name = slopes_image.im.shm
+2026-01-09_11:08:55:861652 [info] dao.c:daoShmShm2Img:711: local name: slopes_image
+2026-01-09_11:08:55:863568 [info] dao.c:daoShmShm2Img:707: shm name = phase_screen.im.shm
+2026-01-09_11:08:55:863577 [info] dao.c:daoShmShm2Img:711: local name: phase_screen
+2026-01-09_11:08:55:864299 [info] dao.c:daoShmShm2Img:707: shm name = dm_phase.im.shm
+2026-01-09_11:08:55:864304 [info] dao.c:daoShmShm2Img:711: local name: dm_phase
+2026-01-09_11:08:55:864863 [info] dao.c:daoShmShm2Img:707: shm name = phase_residuals.im.shm
+2026-01-09_11:08:55:864868 [info] dao.c:daoShmShm2Img:711: local name: phase_residuals
+2026-01-09_11:08:55:865429 [info] dao.c:daoShmShm2Img:707: shm name = normalized_psf.im.shm
+2026-01-09_11:08:55:865433 [info] dao.c:daoShmShm2Img:711: local name: normalized_psf
+2026-01-09_11:08:55:865907 [info] dao.c:daoShmShm2Img:707: shm name = commands.im.shm
+2026-01-09_11:08:55:865911 [info] dao.c:daoShmShm2Img:711: local name: commands
+2026-01-09_11:08:55:866541 [info] dao.c:daoShmShm2Img:707: shm name = residual_modes.im.shm
+2026-01-09_11:08:55:866546 [info] dao.c:daoShmShm2Img:711: local name: residual_modes
+2026-01-09_11:08:55:867046 [info] dao.c:daoShmShm2Img:707: shm name = computed_modes.im.shm
+2026-01-09_11:08:55:867050 [info] dao.c:daoShmShm2Img:711: local name: computed_modes
+2026-01-09_11:08:55:867503 [info] dao.c:daoShmShm2Img:707: shm name = dm_kl_modes.im.shm
+2026-01-09_11:08:55:867508 [info] dao.c:daoShmShm2Img:711: local name: dm_kl_modes
+2026-01-09_11:08:55:867986 [info] dao.c:daoShmShm2Img:707: shm name = norm_flux_pyr_img.im.shm
+2026-01-09_11:08:55:867990 [info] dao.c:daoShmShm2Img:711: local name: norm_flux_pyr_img
+2026-01-09_11:08:55:868431 [info] dao.c:daoShmShm2Img:707: shm name = strehl_ratio.im.shm
+2026-01-09_11:08:55:868434 [info] dao.c:daoShmShm2Img:711: local name: strehl_ratio
+2026-01-09_11:08:55:868879 [info] dao.c:daoShmShm2Img:707: shm name = dm_act.im.shm
+2026-01-09_11:08:55:868882 [info] dao.c:daoShmShm2Img:711: local name: dm_act
+2026-01-09_11:08:55:869343 [info] dao.c:daoShmShm2Img:707: shm name = slm.im.shm
+2026-01-09_11:08:55:869346 [info] dao.c:daoShmShm2Img:711: local name: slm
+2026-01-09_11:08:55:869941 [error] daoShmShm2Img:510: Cannot import shared memory file /tmp/Act2KL.im.shm 
+2026-01-09_11:08:55:869833 [info] dao.c:daoShmShm2Img:707: shm name = KL2Act.im.shm
+2026-01-09_11:08:55:869836 [info] dao.c:daoShmShm2Img:711: local name: KL2Act
+2026-01-09_11:08:55:870569 [info] dao.c:daoShmShm2Img:707: shm name = valid_pixels_mask.im.shm
+2026-01-09_11:08:55:870573 [info] dao.c:daoShmShm2Img:711: local name: valid_pixels_mask
+2026-01-09_11:08:55:870974 [info] dao.c:daoShmShm2Img:707: shm name = bias_image.im.shm
+2026-01-09_11:08:55:870977 [info] dao.c:daoShmShm2Img:711: local name: bias_image
+2026-01-09_11:08:55:871420 [info] dao.c:daoShmShm2Img:707: shm name = reference_psf.im.shm
+2026-01-09_11:08:55:871424 [info] dao.c:daoShmShm2Img:711: local name: reference_psf
+2026-01-09_11:08:55:871918 [info] dao.c:daoShmShm2Img:707: shm name = normalized_ref_psf.im.shm
+2026-01-09_11:08:55:871922 [info] dao.c:daoShmShm2Img:711: local name: normalized_ref_psf
+2026-01-09_11:08:55:872395 [info] dao.c:daoShmShm2Img:707: shm name = reference_image.im.shm
+2026-01-09_11:08:55:872398 [info] dao.c:daoShmShm2Img:711: local name: reference_image
+2026-01-09_11:08:55:872912 [info] dao.c:daoShmShm2Img:707: shm name = ref_img_norm.im.shm
+2026-01-09_11:08:55:872916 [info] dao.c:daoShmShm2Img:711: local name: ref_img_norm
+2026-01-09_11:08:55:873039 [info] dao.c:daoShmShm2Img:707: shm name = npix_valid.im.shm
+2026-01-09_11:08:55:873041 [info] dao.c:daoShmShm2Img:711: local name: npix_valid
+2026-01-09_11:08:55:873213 [info] dao.c:daoShmShm2Img:707: shm name = delay_set.im.shm
+2026-01-09_11:08:55:873215 [info] dao.c:daoShmShm2Img:711: local name: delay_set
+2026-01-09_11:08:55:873330 [info] dao.c:daoShmShm2Img:707: shm name = gain.im.shm
+2026-01-09_11:08:55:873332 [info] dao.c:daoShmShm2Img:711: local name: gain
+2026-01-09_11:08:55:873442 [info] dao.c:daoShmShm2Img:707: shm name = leakage.im.shm
+2026-01-09_11:08:55:873444 [info] dao.c:daoShmShm2Img:711: local name: leakage
+2026-01-09_11:08:55:873550 [info] dao.c:daoShmShm2Img:707: shm name = num_iterations.im.shm
+2026-01-09_11:08:55:873552 [info] dao.c:daoShmShm2Img:711: local name: num_iterations
+2026-01-09_11:08:55:873696 [info] dao.c:daoShmShm2Img:707: shm name = cam1.im.shm
+2026-01-09_11:08:55:873698 [info] dao.c:daoShmShm2Img:711: local name: cam1
+2026-01-09_11:08:55:873804 [info] dao.c:daoShmShm2Img:707: shm name = cam2.im.shm
+2026-01-09_11:08:55:873806 [info] dao.c:daoShmShm2Img:711: local name: cam2
+2026-01-09_11:08:55:873132 [error] daoShmShm2Img:510: Cannot import shared memory file /tmp/KL2PWFS_cube.im.shm 
+2026-01-09_11:08:55:873158 [error] daoShmShm2Img:510: Cannot import shared memory file /tmp/slopes.im.shm 
+2026-01-09_11:08:55:873175 [error] daoShmShm2Img:510: Cannot import shared memory file /tmp/KL2S.im.shm 
+2026-01-09_11:08:55:873189 [error] daoShmShm2Img:510: Cannot import shared memory file /tmp/S2KL.im.shm 
+2026-01-09_11:08:55:873916 [info] dao.c:daoShmShm2Img:707: shm name = cam1Fps.im.shm
+2026-01-09_11:08:55:873918 [info] dao.c:daoShmShm2Img:711: local name: cam1Fps
+2026-01-09_11:08:55:876887 [info] dao.c:daoShmShm2Img:707: shm name = cam2Fps.im.shm
+2026-01-09_11:08:55:876892 [info] dao.c:daoShmShm2Img:711: local name: cam2Fps
+2026-01-09_11:08:55:877399 [info] dao.c:daoShmShm2Img:707: shm name = m2c.im.shm
+2026-01-09_11:08:55:877402 [info] dao.c:daoShmShm2Img:711: local name: m2c
+2026-01-09_11:08:55:877856 [info] dao.c:daoShmShm2Img:707: shm name = dmCmd02.im.shm
+2026-01-09_11:08:55:877859 [info] dao.c:daoShmShm2Img:711: local name: dmCmd02
+2026-01-09_11:08:55:878309 [info] dao.c:daoShmShm2Img:707: shm name = dmCmd03.im.shm
+2026-01-09_11:08:55:878312 [info] dao.c:daoShmShm2Img:711: local name: dmCmd03
+2026-01-09_11:08:55:878754 [info] dao.c:daoShmShm2Img:707: shm name = dm241Map.im.shm
+2026-01-09_11:08:55:878758 [info] dao.c:daoShmShm2Img:711: local name: dm241Map
+2026-01-09_11:08:55:880521 [info] dao.c:daoShmShm2Img:707: shm name = slopes_image.im.shm
+2026-01-09_11:08:55:880527 [info] dao.c:daoShmShm2Img:711: local name: slopes_image
+2026-01-09_11:08:55:881124 [info] dao.c:daoShmShm2Img:707: shm name = phase_screen.im.shm
+2026-01-09_11:08:55:881129 [info] dao.c:daoShmShm2Img:711: local name: phase_screen
+2026-01-09_11:08:55:881348 [info] dao.c:daoShmShm2Img:707: shm name = dm_phase.im.shm
+2026-01-09_11:08:55:881351 [info] dao.c:daoShmShm2Img:711: local name: dm_phase
+2026-01-09_11:08:55:881432 [info] dao.c:daoShmShm2Img:707: shm name = phase_residuals.im.shm
+2026-01-09_11:08:55:881434 [info] dao.c:daoShmShm2Img:711: local name: phase_residuals
+2026-01-09_11:08:55:881510 [info] dao.c:daoShmShm2Img:707: shm name = normalized_psf.im.shm
+2026-01-09_11:08:55:881512 [info] dao.c:daoShmShm2Img:711: local name: normalized_psf
+2026-01-09_11:08:55:881576 [info] dao.c:daoShmShm2Img:707: shm name = commands.im.shm
+2026-01-09_11:08:55:881578 [info] dao.c:daoShmShm2Img:711: local name: commands
+2026-01-09_11:08:55:881696 [info] dao.c:daoShmShm2Img:707: shm name = residual_modes.im.shm
+2026-01-09_11:08:55:881698 [info] dao.c:daoShmShm2Img:711: local name: residual_modes
+2026-01-09_11:08:55:881763 [info] dao.c:daoShmShm2Img:707: shm name = computed_modes.im.shm
+2026-01-09_11:08:55:881765 [info] dao.c:daoShmShm2Img:711: local name: computed_modes
+2026-01-09_11:08:55:881828 [info] dao.c:daoShmShm2Img:707: shm name = dm_kl_modes.im.shm
+2026-01-09_11:08:55:881830 [info] dao.c:daoShmShm2Img:711: local name: dm_kl_modes
+2026-01-09_11:08:55:881895 [info] dao.c:daoShmShm2Img:707: shm name = norm_flux_pyr_img.im.shm
+2026-01-09_11:08:55:881897 [info] dao.c:daoShmShm2Img:711: local name: norm_flux_pyr_img
+2026-01-09_11:08:55:883502 [info] dao.c:daoShmShm2Img:707: shm name = dm_act.im.shm
+2026-01-09_11:08:55:883505 [info] dao.c:daoShmShm2Img:711: local name: dm_act
+2026-01-09_11:08:55:884778 [error] daoShmShm2Img:510: Cannot import shared memory file /tmp/KL2PWFS_cube.im.shm 
+2026-01-09_11:08:55:885173 [error] daoShmShm2Img:510: Cannot import shared memory file /tmp/slopes.im.shm 
+2026-01-09_11:08:55:885189 [error] daoShmShm2Img:510: Cannot import shared memory file /tmp/KL2S.im.shm 
+2026-01-09_11:08:55:885204 [error] daoShmShm2Img:510: Cannot import shared memory file /tmp/S2KL.im.shm 
+2026-01-09_11:08:55:884497 [info] dao.c:daoShmShm2Img:707: shm name = slm.im.shm
+2026-01-09_11:08:55:884503 [info] dao.c:daoShmShm2Img:711: local name: slm
+2026-01-09_11:08:55:884635 [info] dao.c:daoShmShm2Img:707: shm name = ref_img_norm.im.shm
+2026-01-09_11:08:55:884638 [info] dao.c:daoShmShm2Img:711: local name: ref_img_norm
+2026-01-09_11:08:55:884717 [info] dao.c:daoShmShm2Img:707: shm name = npix_valid.im.shm
+2026-01-09_11:08:55:884718 [info] dao.c:daoShmShm2Img:711: local name: npix_valid
+2026-01-09_11:08:55:885222 [info] dao.c:daoShmShm2Img:707: shm name = delay_set.im.shm
+2026-01-09_11:08:55:885224 [info] dao.c:daoShmShm2Img:711: local name: delay_set
+2026-01-09_11:08:55:885296 [info] dao.c:daoShmShm2Img:707: shm name = gain.im.shm
+2026-01-09_11:08:55:885298 [info] dao.c:daoShmShm2Img:711: local name: gain
+2026-01-09_11:08:55:885363 [info] dao.c:daoShmShm2Img:707: shm name = leakage.im.shm
+2026-01-09_11:08:55:885365 [info] dao.c:daoShmShm2Img:711: local name: leakage
+2026-01-09_11:08:55:885476 [info] dao.c:daoShmShm2Img:707: shm name = num_iterations.im.shm
+2026-01-09_11:08:55:885478 [info] dao.c:daoShmShm2Img:711: local name: num_iterations
+2026-01-09_11:08:55:885547 [info] dao.c:daoShmShm2Img:707: shm name = cam1.im.shm
+2026-01-09_11:08:55:885549 [info] dao.c:daoShmShm2Img:711: local name: cam1
+2026-01-09_11:08:55:885617 [info] dao.c:daoShmShm2Img:707: shm name = cam2.im.shm
+2026-01-09_11:08:55:885619 [info] dao.c:daoShmShm2Img:711: local name: cam2
+2026-01-09_11:08:55:885682 [info] dao.c:daoShmShm2Img:707: shm name = cam1Fps.im.shm
+2026-01-09_11:08:55:885684 [info] dao.c:daoShmShm2Img:711: local name: cam1Fps
+2026-01-09_11:08:55:885750 [info] dao.c:daoShmShm2Img:707: shm name = cam2Fps.im.shm
+2026-01-09_11:08:55:885752 [info] dao.c:daoShmShm2Img:711: local name: cam2Fps
+b'\rCommand error CMD_NOT_DEFINED'
+INIT OK
+2026-01-09_11:08:55:885815 [info] dao.c:daoShmShm2Img:707: shm name = m2c.im.shm
+2026-01-09_11:08:55:885817 [info] dao.c:daoShmShm2Img:711: local name: m2c
+2026-01-09_11:08:56:493922 [info] dao.c:daoShmShm2Img:707: shm name = cam1.im.shm
+2026-01-09_11:08:56:493933 [info] dao.c:daoShmShm2Img:711: local name: cam1
+2026-01-09_11:08:56:494062 [info] dao.c:daoShmShm2Img:707: shm name = cam2.im.shm
+2026-01-09_11:08:56:494065 [info] dao.c:daoShmShm2Img:711: local name: cam2
+2026-01-09_11:08:56:494138 [info] dao.c:daoShmShm2Img:707: shm name = cam1Fps.im.shm
+2026-01-09_11:08:56:494140 [info] dao.c:daoShmShm2Img:711: local name: cam1Fps
+2026-01-09_11:08:56:494417 [info] dao.c:daoShmShm2Img:707: shm name = cam2Fps.im.shm
+2026-01-09_11:08:56:494420 [info] dao.c:daoShmShm2Img:711: local name: cam2Fps
+Laser is Accessible
+SLM is open
+b'\rCommand error CMD_NOT_DEFINED'
+INIT OK
+2026-01-09_11:08:56:494879 [info] dao.c:daoShmShm2Img:707: shm name = slm.im.shm
+2026-01-09_11:08:56:494882 [info] dao.c:daoShmShm2Img:711: local name: slm
+2026-01-09_11:09:13:196123 [info] dao.c:daoShmShm2Img:707: shm name = cam1.im.shm
+2026-01-09_11:09:13:196137 [info] dao.c:daoShmShm2Img:711: local name: cam1
+2026-01-09_11:09:13:196281 [info] dao.c:daoShmShm2Img:707: shm name = cam2.im.shm
+2026-01-09_11:09:13:196283 [info] dao.c:daoShmShm2Img:711: local name: cam2
+2026-01-09_11:09:13:196366 [info] dao.c:daoShmShm2Img:707: shm name = cam1Fps.im.shm
+2026-01-09_11:09:13:196368 [info] dao.c:daoShmShm2Img:711: local name: cam1Fps
+2026-01-09_11:09:13:196548 [info] dao.c:daoShmShm2Img:707: shm name = cam2Fps.im.shm
+2026-01-09_11:09:13:196551 [info] dao.c:daoShmShm2Img:711: local name: cam2Fps
+Laser is Accessible
+SLM is open
+Were are in Geneva
+
+In [2]: %runcell -n 'Setting DM to flat' '/home/ristretto-dao/optlab-master/PROJECTS_3/RISTRETTO/Banc AO/scripts_with_dao/dao_calibrations_file.py'
+---------------------------------------------------------------------------
+AttributeError                            Traceback (most recent call last)
+File ~/optlab-master/PROJECTS_3/RISTRETTO/Banc AO/scripts_with_dao/dao_calibrations_file.py:67
+     52 dm_papy_shm = shm.dm_papy_shm
+     55 # #%% Accessing Devices
+     56 
+     57 # # Initialize Cameras
+   (...)
+     65 
+     66 # Compute and display Pupil Data on SLM
+---> 67 data_slm = compute_data_slm()
+     68 slm.set_data(data_slm)
+     69 time.sleep(wait_time)
+
+File ~/optlab-master/PROJECTS_3/RISTRETTO/Banc AO/src/utils.py:106, in compute_data_slm(data_dm, data_phase_screen, data_dm_flat, setup, **kwargs)
+    103         raise ValueError("No setup provided and no default registered.")
+    104     setup = DEFAULT_SETUP
+--> 106 data_pupil_inner = kwargs.get("data_pupil_inner", setup.data_pupil_inner)
+    107 data_pupil_outer = kwargs.get("data_pupil_outer", setup.data_pupil_outer)
+    108 pupil_mask = kwargs.get("pupil_mask", setup.pupil_mask)
+
+AttributeError: 'DAOSetup' object has no attribute 'data_pupil_inner'
+
+In [3]: 
